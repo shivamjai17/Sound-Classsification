2c855c53d96086bf4517a508d5c5051bb0f5a595
shivam jaiswal <shivamjaiswal17032000@gmail.com>
2021-12-03 21:31:28 +0530

    sound

---
+++ README.md
@@ -0,0 +1,12 @@
+# Flask-based webapp for sound classification with a neural network
+
+
+
+A sound recogniton webapp that uses a convolutional neural network trained on a few thosuand examples to make predictions in 10 categories.
+
+To see the process of data collection and model development, visit [sourcing-sound](https://github.com/crunker99/sourcing-sound).
+
+Credit to 
+[Web Audio API](https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html) for many of the audio input features.  
+
+
---
+++ app.py
@@ -0,0 +1,45 @@
+from logging import debug
+from src.helpers import featurize_predict
+
+from flask import Flask, url_for, request, render_template, Response, jsonify
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+import librosa
+from tensorflow.python.keras.models import load_model
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+app = Flask(__name__)
+
+MODEL = load_model(filepath='models/model1.hdf5')
+MODEL.summary()
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get user's audio from post request
+        print("audio finished")
+        if 'file' not in request.files:
+            app.logger.debug("[server] no file part")
+            return '[server] no file part'
+        r = request.files['file']
+        try:
+            signal, _ = librosa.load(r)
+        except ValueError:
+            return jsonify(result="inputError")
+
+        mp = featurize_predict(signal, MODEL)
+        return jsonify(result=mp['result'], probability=mp['probability'], spec=mp['spec'])
+
+    return None
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(debug=True)
